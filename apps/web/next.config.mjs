27b13c25a5567c842f082d48b8fbe60cc8af7078
Tomáš Witek <tomas@witek.cz>
2024-01-23 14:40:38 +0100

    Configure i18n support

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ["en", "cs"],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "cs",
+    localeDetection: false,
+  },
+};
 
 export default nextConfig;
