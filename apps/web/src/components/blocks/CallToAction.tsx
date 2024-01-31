60a0607f6853206aa4be60d44154d71ed8d62208
Tomáš Witek <tomas@witek.cz>
2024-01-31 13:52:46 +0100

    Optimize calltoaction responsive layout

--- apps/web/src/components/blocks/CallToAction.tsx
+++ apps/web/src/components/blocks/CallToAction.tsx
@@ -28,7 +28,7 @@ export function CallToAction(props: Props) {
   return (
     <div className="bg-bg">
       <div className="mx-auto max-w-7xl px-6 lg:px-8 p-10">
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <div className="grid grid-cols-1 gap-x-16 gap-y-6 lg:grid-cols-2">
           <div>
             <h3
               className="text-primary text-5xl mb-6"
@@ -39,19 +39,19 @@ export function CallToAction(props: Props) {
               dangerouslySetInnerHTML={{__html: content}}
             />
           </div>
-          <div className="flex justify-end">
+          <div className="flex lg:justify-end">
             {buttons.map((button, index) => (
               <Link
                 key={index}
                 href="/"
-                className="group border-2 border-transparent hover:border-gray-400"
+                className="group border-2 border-transparent hover:border-gray-400 w-full"
               >
-                <div className="relative">
+                <div className="relative w-full h-64">
                   <Image
                     alt={button.label}
                     src={button.image}
-                    width={450}
-                    height={400}
+                    fill
+                    className="object-cover"
                   />
                   <div className="bg-bg-light border-2 border-transparent text-gray-300 inline-block py-3 px-5 absolute right-4 -bottom-4 group-hover:border-gray-400">
                     {button.label}
