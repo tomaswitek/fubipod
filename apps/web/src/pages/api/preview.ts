84e4c24387ab1d3596bac0e9175e9644bb153d33
Tomáš Witek <tomas@witek.cz>
2024-02-13 11:50:41 +0100

    Add redirect to preview url

--- apps/web/src/pages/api/preview.ts
+++ apps/web/src/pages/api/preview.ts
@@ -2,7 +2,7 @@ import {NextApiHandler} from "next";
 
 const handler: NextApiHandler = (req, res) => {
   res.setPreviewData({});
-  res.end("Preview mode enabled");
+  res.redirect("/");
 };
 
 export default handler;
