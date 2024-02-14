bafbc2551ec18472e3b9e2973367ebf83bb5955c
Tomáš Witek <tomas@witek.cz>
2024-02-14 12:31:39 +0100

    Specify seo types

--- packages/types/seo.ts
+++ packages/types/seo.ts
@@ -2,4 +2,11 @@ export interface Seo {
   id: string;
   title: string;
   description: string;
+  og_image: string;
+  twitter_image: string;
+  no_follow: boolean;
+  no_index: boolean;
+  canonical_url: string;
+  sitemap_change_frequency: string;
+  sitemap_priority: string;
 }
