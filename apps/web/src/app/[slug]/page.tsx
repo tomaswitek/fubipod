d71bf3825b7b697b4c840d3071ee082bff38c2c4
Tomáš Witek <tomas@witek.cz>
2024-02-14 12:31:07 +0100

    SEO metadata example

--- apps/web/src/app/[slug]/page.tsx
+++ apps/web/src/app/[slug]/page.tsx
@@ -2,10 +2,35 @@ import {Page} from "@/components/Page";
 import {Header} from "@/components/Header";
 import {Footer} from "@/components/Footer";
 import {client} from "api";
+import {ResolvingMetadata, Metadata} from "next";
 
 export async function generateStaticParams() {
   const pages = await client.getPages();
   return pages.map(({slug}) => ({slug}));
+}
+
+type Props = {
+  params: {slug: string};
+  searchParams: {[key: string]: string | string[] | undefined};
+};
+
+export async function generateMetadata(
+  {params, searchParams}: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const {slug} = params;
+
+  const page = await client.getPage(slug);
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: page?.title,
+    openGraph: {
+      images: ["/some-specific-page-image.jpg", ...previousImages],
+    },
+  };
 }
 
 export default function SlugPage(props: {params: {slug: string}}) {
