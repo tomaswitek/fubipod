77315f40fce3025767bcc7f9a0d50f36d275bd93
Tomáš Witek <tomas@witek.cz>
2024-02-18 16:06:05 +0100

    Replace fonts

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,24 @@
 import type {Metadata} from "next";
 import {Bebas_Neue} from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const font = Bebas_Neue({subsets: ["latin"], weight: ["400"]});
+const font = localFont({
+  src: [
+    {
+      path: "../fonts/BebasNeue-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/BebasNeue-Bold.woff2",
+      weight: "700",
+      style: "bold",
+    },
+  ],
+});
+
+// const font = Bebas_Neue({subsets: ["latin"], weight: ["400"]});
 
 export const metadata: Metadata = {
   title: "FUBI",
